f1b7e40aa862fb4f77ab62e6f1ec793e5b7bc6bb
Unknown <brockljakub@gmail.com>
2016-11-03 00:00:10 +0100

    Created Class Employee

---
+++ Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace XA01
+{ 
+  abstract class Employee
+    {
+        public abstract string Name { get; set; }
+        public abstract int DailyWage { get; set; }
+       
+     
+    }
+}
